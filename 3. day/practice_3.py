8bcebbd5f25c82198c018fef3998766bd7861b1a
kalaposlany <bogar412@gmail.com>
2026-02-26 15:25:16 +0100

    2.day

---
+++ 3. day/practice_3.py
@@ -0,0 +1,172 @@
+""""""
+"""---------------------------------------------------------------------------------------------------------------------
+1. feladat
+Adottak a következő szavak: két, macska, van.
+Jelenítsd meg őket egy mondatban! (Két macska van.)
+Jelenítsd meg őket egy mondatban, de számot használj! (2 monitor van.)
+Mindkét esetben legalább két-két módszerrel printelj (pl. string összefűzés, f-string vagy print paraméterek).
+"""
+
+
+
+"""---------------------------------------------------------------------------------------------------------------------
+2. feladat
+Add meg egy felhasználótól bekért szó páratlan helyen lévő betűjeit és írasd őket ki! (pl. alma esetén a megoldás a,m)
+"""
+
+
+
+"""---------------------------------------------------------------------------------------------------------------------
+3. feladat
+Készíts egy öt elemű felhasználótól bekért törtszám listát! Jelenítsd meg a képernyőn a listát!
+"""
+
+
+
+"""---------------------------------------------------------------------------------------------------------------------
+4. feladat
+Ellenőrizd, hogy megegyezik-e egy lista első és utolsó eleme! Írasd ki a megfelelő boolean értéket a képernyőre!
+A listát Te határozhatod meg.
+"""
+
+
+
+"""---------------------------------------------------------------------------------------------------------------------
+5. feladat
+Adott egy lista, mely számokat tartalmaz. Jelenítsd meg a listából azokat a számokat a képernyőn, melyek oszthatók öttel.
+Viszont, ha a szám nagyobb mint 100, akkor, azt hagyd figyelmen kívül és lépj a következő számra.
+Ha 250-nél is nagyobb a szám, fejezd be a kiíratást.
+A számok: 8, 10, 75, 149, 64, 89, 225, 5, 145, 260, 1, 50
+"""
+
+
+
+"""---------------------------------------------------------------------------------------------------------------------
+6. feladat
+Adott egy szám. (Te választhatod ki.) Számold meg, hogy hány számjegyből áll!
+Próbáld meg while ciklus segítségével is megoldani a feladatot!
+"""
+
+
+
+"""---------------------------------------------------------------------------------------------------------------------
+7. feladat
+Írj egy szkriptet, ami bekér a felhasználótól egy egész számot, majd írasd ki ennek a számnak az egész osztóit!
+"""
+
+
+
+"""---------------------------------------------------------------------------------------------------------------------
+8. feladat
+'''
+I. Készíts egy szkriptet, ami kijelzi, hogy a felhasználótól bekért szó palindróma-e?
+II. Készíts egy szkriptet, ami kijelzi, hogy a felhasználótól bekért mondat palindróma-e?
+Palindróma az, ami visszafele olvasva is ugyanazt adja, mint az eredeti. Pl. szó esetén: kék, mondat esetén: Indul a görög aludni.
+"""
+
+
+
+"""---------------------------------------------------------------------------------------------------------------------
+9. feladat
+Írd ki a Fibonacci sor első tíz elemét!
+A sor kezdődjön 0-val és 1-gyel.
+(Fibonacci sor: két egymást követő elem összege egyenlő a követő elemmel. 0, 1, 1, 2, 3, 5, 8,...
+"""
+
+
+
+"""---------------------------------------------------------------------------------------------------------------------
+10. feladat
+Rajzold ki az alábbi ábrát:
++
+++
++++
+++++
++++++
+"""
+
+
+
+"""---------------------------------------------------------------------------------------------------------------------
+11. feladat
+Adott egy lista, amelyben a bárányok hallgatnak (False) vagy a bárányok bégetnek (True).
+Számoljuk meg, hogy hány bárány béget és hány bárány hallgat!
+"""
+lambs = [True, True, False, True, False, False, True, False, False, True, False, True]
+
+
+
+"""---------------------------------------------------------------------------------------------------------------------
+12. feladat
+Kérjünk be a felhasználótól egy stringet és printeljük neki vissza úgy, hogy minden egyes karaktert megduplázunk.
+Pl.:
+String      -> SSttrriinngg
+Hello World -> HHeelllloo  WWoorrlldd
+1234!_      -> 11223344!!__
+"""
+
+
+
+"""---------------------------------------------------------------------------------------------------------------------
+13. feladat
+Kérjünk be a felhasználótól legalább két szót, alakítsuk át CamelCase formára és printeljük ki.
+Pl.:
+camel case -> CamelCase
+nagy medve -> NagyMedve
+"""
+
+
+
+"""---------------------------------------------------------------------------------------------------------------------
+14. feladat
+Generáljunk két random (véletlen) pozitív természetes számot 3 és 20 között  és rajzoljunk "$" karaterrel négyzetet
+vagy "*" karakterrel téglalapot (mindegy, hogy álló vagy fekvő).
+
+Segítség random szám generálásához: https://www.w3schools.com/python/ref_random_randint.asp
+
+Pl.:
+4, 5
+* * * *
+* * * *
+* * * *
+* * * *
+* * * *
+
+3, 3
+$  $  $
+$  $  $
+$  $  $
+
+"""
+
+
+
+"""---------------------------------------------------------------------------------------------------------------------
+15. feladat
+Generálj egy random számot 10 és 20 között, majd készíts egy listát, amelynek annyi eleme van,
+mint ez a random szám. A lista elemei is random számok legyenek 1 és 100 között.
+Nyomtasd ki a listát.
+Ha a lista páratlan számú elemet tartalmaz, akkor jelenítsd meg a középső elemet, majd töröld is ki a listából.
+Ha a lista páros számú elemet tartalmaz, akkor jelenítsd meg a két középső elemet, majd töröld is ki őket a listából.
+Az elem (vagy elemek) kitörlése után is nyomtasd ki a listát.
+
+Segítség random szám generáláshoz: https://www.w3schools.com/python/ref_random_randint.asp
+"""
+
+
+
+"""---------------------------------------------------------------------------------------------------------------------
+16. feladat
+A szkripted számolja és írja ki, hogy hány év múlva lesz egy apa kétszer olyan idős, mint a fia.
+Az adatokat a felhasználótól kérd be!
+A szkripted kezelje azt is, ha ez a duplázódás a múltban volt.
+"""
+
+
+
+"""---------------------------------------------------------------------------------------------------------------------
+17. feladat
+Írj egy kő - papír - olló játékot!
+Két játékos egymás után választhat, a szokásos szabályok mentén.
+Írd ki, hogy melyik játékos nyert, illetve, hogy szeretnék-e tovább folytatni a játékot?
+"""
